d123f83f2a762f1922980f2316274488dd6c1a98
AxelP312 <axepetaxe@gmail.com>
2020-09-01 10:51:22 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace hangman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //File.WriteAllText("text.txt", "THE CAKE IS A LIE");
+            //string ahh = File.ReadAllText("text.txt");
+            //Console.WriteLine(ahh);
+            
+
+            // string[] words = {"hat", "coat", "Pants"};
+            
+            //string[] words = File.ReadAllText("word.txt");
+
+            // File.WriteAllLines("words.txt", words);
+
+
+            if(File.Exists("words.txt"))
+            {
+                string[] words = File.ReadAllLines("words.txt");
+                Console.WriteLine(words[0]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
